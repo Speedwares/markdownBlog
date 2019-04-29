e73e67c57f7b3f6d83f5f47692d516bf1a0cb53e
overwatch <ovuobasampson@gmail.com>
2019-04-28 17:32:36 -0700

    updated blog

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import {Link} from 'gatsby'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function Template({data}) {
   const post = data.markdownRemark
   return (
+    <Layout>
+      <SEO title= {post.frontmatter.title} />
     <div>
       <Link to="/blog"> Go Back </Link>
       <hr />
       <h2> {post.frontmatter.title}</h2>
       <br />
-      <h4>Posted by {post.frontmatter.author} on {post.node.frontmatter.date}</h4>
+      <h4>Posted by {post.frontmatter.author} on {post.frontmatter.date}</h4>
       <br />
       <div dangerouslySetInnerHTML={{__html: post.html }}/>
 
 
     </div>
+    </Layout>
   )
 
 
@@ -25,7 +30,10 @@ query BlogPostPath($path: String!){
    markdownRemark(frontmatter: { path: { eq: $path } }) {
      html
      frontmatter{
-     
+       path
+       title
+       author
+       date
      }
    
    }
