aaad4b72aaea754c542744252be145ee1214d1e3
overwatch <ovuobasampson@gmail.com>
2019-04-28 17:33:19 -0700

    added menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,1 +1,23 @@
+import React from "react"
+import {Link} from "gatsby"
 
+
+const MENU = () => (
+    <div style={{
+      background: "#f47298",
+      paddingTop: "10px"
+    }}>
+      <ul style={{
+        listStyle: "none",
+        display: "flex",
+        justifyContent: "space-evenly"
+      }}>
+        <li> <Link to="/"> Home </Link>  </li>
+        <li> <Link to="/about"> About </Link>  </li>
+        <li> <Link to="/blog"> Blog </Link>  </li>
+      </ul>
+
+    </div>
+)
+
+export default MENU
